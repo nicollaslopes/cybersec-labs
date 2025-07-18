805743a645fb742731f51c3af5c8d16dccb3f23d
nicollas.lopes <nicollaslopesn@gmail.com>
2025-07-18 08:15:49 -0300

    style: changing favicon and title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laboratório de Falhas Web</title>
+    <title>Pentesting Lab</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Poppins:wght@600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
